859db867e79daf61380f9dafe03f90ae95fe0665
wangjr01 <wangjr01@mingyuanyun.com>
2023-10-31 23:35:47 +0800

    user service init

---
+++ services/user-service/src/main/java/com/simple/chicken/user/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.simple.chicken.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName UserServiceApplication
+ * @Description TODO
+ * @Author wjr
+ * @Date 2023/10/31 23:31
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class,args);
+    }
+}
