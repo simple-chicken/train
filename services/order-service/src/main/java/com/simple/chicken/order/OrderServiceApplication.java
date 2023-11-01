841955c0b7c0fc17d802fc4bdb66a6e612a59155
wjr <junru.wang@ly.com>
2023-11-01 11:27:50 +0800

    feature：服务初始化

---
+++ services/order-service/src/main/java/com/simple/chicken/order/OrderServiceApplication.java
@@ -0,0 +1,20 @@
+package com.simple.chicken.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName OrderServiceApplication
+ * @Description TODO
+ * @Author wjr
+ * @Date 2023/11/1 10:07
+ */
+
+
+@SpringBootApplication
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class,args);
+    }
+
+}
